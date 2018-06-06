d8077d0039110b2f558d69f95e13f407600d7df1
anil1581994 <anilrspatil1992@gmail.com>
2018-06-06 13:14:32 +0530

    swagger usage

---
+++ SpringBoot-Swagger2/src/main/java/com/bridgelabz/HelloController.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/api/javainuse")
+	public String sayHello() {
+		return "Swagger Hello World anil get ready to get documentation of your api";
+	}
+}
---
+++ SpringBoot-Swagger2/src/main/java/com/bridgelabz/SpringBootSwagger2Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger2Application.class, args);
+		//http://www.javainuse.com/spring/boot_swagger
+	}
+}
